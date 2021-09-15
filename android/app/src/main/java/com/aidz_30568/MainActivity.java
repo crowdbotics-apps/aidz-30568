8896fc703bc9a26369379bab0776ae85170515e4
Crowdbotics <team@crowdbotics.com>
2021-09-15 09:14:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/aidz_30568/MainActivity.java
@@ -0,0 +1,15 @@
+package com.aidz_30568;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "aidz_30568";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "aidz_30568Identifier",
+  url: "https://aidz_30568Identifier.botics.co",
+  api: "https://aidz_30568Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
